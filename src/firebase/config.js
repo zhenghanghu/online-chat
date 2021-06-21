a4834f7425f41111c0976502f3e4f010b5a5e8ae
zhenghang hu <hu4396@terpmail.umd.edu>
2021-06-21 17:01:12 -0400

    created welcome page

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuCMq-G3UyquAsV9zndxqcgPzAQng4eoo",
+  authDomain: "memo-miaoplus.firebaseapp.com",
+  projectId: "memo-miaoplus",
+  storageBucket: "memo-miaoplus.appspot.com",
+  messagingSenderId: "795370179971",
+  appId: "1:795370179971:web:6b63dc4df4e97f77b87ab6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
