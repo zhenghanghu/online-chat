0aaf0fefa740930e139ce7df338e3b951db080b9
zhenghang hu <hu4396@terpmail.umd.edu>
2021-06-21 22:43:26 -0400

    added logout composable

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+      <div>
+          <p>Hey there... display name here</p>
+          <p class="email">Currently logged in as ... email</p>
+      </div>
+      <button @click="handleClick">Logout</button>
+  </nav>
+</template>
+
+<script>
+import useLogout from '../composables/useLogout'
+
+export default {
+  setup(){
+    const { logout, error} = useLogout()
+    const handleClick = async () => {
+      await logout()
+      if(!error.value){
+        console.log("user logged out")
+      }
+    }
+    return {handleClick}
+  }
+}
+</script>
+
+<style>
+
+</style>
