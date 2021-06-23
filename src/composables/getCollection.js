702f5902dd4fb5975552dc5d045ad598f30a87cd
zhenghang hu <hu4396@terpmail.umd.edu>
2021-06-23 01:35:39 -0400

    added live-listen with onSnapshot

---
+++ src/composables/getCollection.js
@@ -0,0 +1,24 @@
+import {ref} from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getCollection = (collection) => {
+    const documents = ref(null)
+    const error = ref(null)
+
+    let collectionRef = projectFirestore.collection(collection)
+        .orderBy('createdAt')
+
+    collectionRef.onSnapshot((snap) => {
+        let results = []
+        snap.docs.forEach((doc)=>{
+            console.log(doc.data())
+            doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+        })
+        documents.value = results
+        error.value = null
+    })
+    
+    return {documents, error}
+}
+
+export default getCollection
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
         <Navbar />
+        <ChatWindow />
         <NewChatForm />
     </div>
 </template>
@@ -8,9 +9,10 @@
 <script>
 import Navbar from '../components/Navbar.vue'
 import NewChatForm from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 
 export default {
-    components: { Navbar, NewChatForm }
+    components: { Navbar, NewChatForm, ChatWindow }
 }
 </script>
 
